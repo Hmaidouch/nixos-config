20132139f10e768d8fa18b317926e9bc8947f0fb
Benattia ahmed <benattia2@gmail.com>
2026-06-17 19:23:40 +0100

    cccc

--- base.nix
+++ base.nix
@@ -9,36 +9,7 @@
     home.homeDirectory = "/home/benattia";
     home.stateVersion = "26.05";
 
-    home.file.".config/hypr/hyprland.lua".text = ''
-      hl.config({
-        input = {
-            kb_layout = "fr,ara",
-            kb_options = "grp:alt_shift_toggle"
-        },
-
-        monitor = {
-            "Unknown-1,1920x1080@60,0x0,1"
-        }
-    })
-
-    hl.bind("SUPER + Q", hl.dsp.window.close())
-    hl.bind("SUPER + T", hl.dsp.exec_cmd("alacritty"))
-    hl.bind("SUPER + B", hl.dsp.exec_cmd("firefox"))
-    hl.bind("SUPER + E", hl.dsp.exec_cmd("nemo"))
-    hl.bind("SUPER + C", hl.dsp.exec_cmd("code"))
-
-    hl.bind(
-        "SUPER + mouse:272",
-        hl.dsp.window.drag(),
-        { mouse = true }
-    )
-
-    hl.bind(
-        "SUPER + mouse:273",
-        hl.dsp.window.resize(),
-        { mouse = true }
-    )
-  '';
+    xdg.configFile."hypr/hyprland.lua".source = ./hyprland.lua;
 
   };
 
