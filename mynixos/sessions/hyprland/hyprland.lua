259e0823e02f56926704688d97925b5f50e3f8e1
Benattia ahmed <benattia2@gmail.com>
2026-06-18 13:36:23 +0100

    gfgfg

--- mynixos/sessions/hyprland/hyprland.lua
+++ mynixos/sessions/hyprland/hyprland.lua
@@ -36,5 +36,5 @@ hl.bind(
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 
-hl.env = XCURSOR_SIZE,24
-hl.env = HYPRCURSOR_SIZE,24
+hl.env = XCURSOR_SIZE,10
+hl.env = XCURSOR_THEME,Posy_Cursor
